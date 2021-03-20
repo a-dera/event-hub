8db0421b9fcf20bd1e2037172963ed5f291b43a7
Amédée DERA <dera.amedee@gmail.com>
2021-03-20 10:08:58 +0000

    Intégration de Firebase ⚡️

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,27 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyCNycN7H1qrqj-YaeJmrv34XdMCavJ2N60",
+    authDomain: "eventhub-210f2.firebaseapp.com",
+    projectId: "eventhub-210f2",
+    storageBucket: "eventhub-210f2.appspot.com",
+    messagingSenderId: "536601603659",
+    appId: "1:536601603659:web:2010968023a469c162a7ce"
+  };
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule,
+    AngularFireStorageModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
