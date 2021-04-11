080e58225ea75a8ce97b510319c73d226afc7070
Amédée DERA <dera.amedee@gmail.com>
2021-04-11 17:34:23 +0000

    Tentavie échouée d'implémentation de ''image file upload"

--- src/app/event-create/event-create.page.ts
+++ src/app/event-create/event-create.page.ts
@@ -4,6 +4,9 @@ import { ToastService } from '../services/toast.service';
 import { Router } from '@angular/router';
 // import { Key } from 'selenium-webdriver';
 import { CrudService } from '../services/crud.service';
+//import { ImagePicker } from '@ionic-native/image-picker/ngx';
+//import { WebView } from '@ionic-native/ionic-webview/ngx'
+import { LoadingController, ToastController } from '@ionic/angular';
 
 
 @Component({
@@ -21,33 +24,94 @@ export class EventCreatePage implements OnInit {
   lieu: string;
   prix: string;
   organisateur: string;
+  image: any;
       
   constructor(
     private crudService: CrudService,
     private router: Router,
     private toastservice: ToastService
+    //private webview: WebView,
+    //private imagePicker: ImagePicker,
+    //public toastCtrl: ToastController,
+    //public loadingCtrl: LoadingController
     ) { }
 
   ngOnInit() {
+    //this.resetFields();
   }
 
+  /*resetFields(){
+    this.image = "https://images.pexels.com/photos/793759/pexels-photo-793759.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260";
+  }*/
+
     add(libel, description, date, heure, lieu, prix, organisateur){
-    let data = {
-      //$key: key.value,
-      libel:libel.value,
-      description: description.value,
-      date:  date.value, 
-      heure: heure.value, 
-      lieu: lieu.value, 
-      prix: prix.value, 
-      organisateur: organisateur.value
+      let data = {
+        //$key: key.value,
+        libel:libel.value,
+        description: description.value,
+        date:  date.value, 
+        heure: heure.value, 
+        lieu: lieu.value, 
+        prix: prix.value, 
+        organisateur: organisateur.value,
+        image: this.image
+      }
+        this.crudService.create(data)
+        .then((res) => {
+          // Do something here
+          this.router.navigate(['tabs/event']);
+        }).catch((error) => {
+          this.toastservice.showToast(error.message, 2000);
+        })
     }
-      this.crudService.create(data)
-      .then((res) => {
-        // Do something here
-        this.router.navigate(['tabs/event']);
-      }).catch((error) => {
-        this.toastservice.showToast(error.message, 2000);
-      })
+
+   /* openImagePicker(){
+    this.imagePicker.hasReadPermission()
+    .then((result) => {
+      if(result == false){
+        // no callbacks required as this opens a popup which returns async
+        this.imagePicker.requestReadPermission();
+      }
+      else if(result == true){
+        this.imagePicker.getPictures({
+          maximumImagesCount: 1
+        }).then(
+          (results) => {
+            for (var i = 0; i < results.length; i++) {
+              this.uploadImageToFirebase(results[i]);
+            }
+          }, (err) => console.log(err)
+        );
+      }
+    }, (err) => {
+      console.log(err);
+    });
   }
+
+  async uploadImageToFirebase(image){
+    const loading = await this.loadingCtrl.create({
+      message: 'Please wait...'
+    });
+    const toast = await this.toastCtrl.create({
+      message: 'Image was updated successfully',
+      duration: 3000
+    });
+    this.presentLoading(loading);
+    let image_src = this.webview.convertFileSrc(image);
+    let randomId = Math.random().toString(36).substr(2, 5);
+
+    //uploads img to firebase storage
+    this.crudService.uploadImage(image_src, randomId)
+    .then(photoURL => {
+      this.image = photoURL;
+      loading.dismiss();
+      toast.present();
+    }, err =>{
+      console.log(err);
+    })
+  }
+
+  async presentLoading(loading) {
+    return await loading.present();
+  }*/
 }
--- src/app/services/crud.service.ts
+++ src/app/services/crud.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { Router } from "@angular/router";
+//import 'firebase/storage';
+//import * as firebase from 'firebase/app';
 
 export class Event {
   //$key: string;
@@ -11,6 +13,7 @@ export class Event {
   lieu: string;
   prix: string;
   organisateur: string;
+  //image: any;
 }
 
 @Injectable({
@@ -50,5 +53,36 @@ export class CrudService {
   delete(id: string) {
     this.ngFirestore.doc('events/' + id).delete();
   }
+
+  /*encodeImageUri(imageUri, callback) {
+    var c = document.createElement('canvas');
+    var ctx = c.getContext("2d");
+    var img = new Image();
+    img.onload = function () {
+      var aux:any = this;
+      c.width = aux.width;
+      c.height = aux.height;
+      ctx.drawImage(img, 0, 0);
+      var dataURL = c.toDataURL("image/jpeg");
+      callback(dataURL);
+    };
+    img.src = imageUri;
+  };
+
+  uploadImage(imageURI, randomId){
+    return new Promise<any>((resolve, reject) => {
+      let storageRef = firebase.storage().ref();
+      let imageRef = storageRef.child('image').child(randomId);
+      this.encodeImageUri(imageURI, function(image64){
+        imageRef.putString(image64, 'data_url')
+        .then(snapshot => {
+          snapshot.ref.getDownloadURL()
+          .then(res => resolve(res))
+        }, err => {
+          reject(err);
+        })
+      })
+    })
+  }*/
 
 }
